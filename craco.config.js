56289d1f1cd1449cb9466cd1ac38fda3a52dfe06
loong <bzlw@pm.me>
2025-06-21 21:41:29 -0700

    init rdkit

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,6 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   webpack: {
     configure: (config) => {
@@ -7,6 +10,20 @@ module.exports = {
         fs: false,
         crypto: false,
       };
+      // Add CopyWebpackPlugin to copy RDKit_minimal.wasm
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.resolve(
+                __dirname,
+                'node_modules/@rdkit/rdkit/dist/RDKit_minimal.wasm'
+              ),
+              to: 'static/js', // Output to the CRA build folder
+            },
+          ],
+        })
+      );
       return config;
     },
   },
--- src/rdkit.d.ts
+++ src/rdkit.d.ts
@@ -1,10 +1,12 @@
 import type { RDKitModule, RDKitLoader } from '@rdkit/rdkit';
 
+declare module '@rdkit/rdkit/dist/RDKit_minimal.js';
+
 declare global {
-  interface Window {
-    RDKit?: RDKitModule;
-    initRDKitModule: RDKitLoader;
-  }
+    interface Window {
+        RDKit?: RDKitModule;
+        initRDKitModule: RDKitLoader;
+    }
 }
 
-export {};
+export { };
